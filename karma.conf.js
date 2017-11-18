8ded8947733aacf31c51c77e95313985c999d91a
tdeekens <nerd@tdeekens.name>
2017-11-18 15:00:47 +0100

    chore(package): remove webpack

--- karma.conf.js
+++ karma.conf.js
@@ -3,21 +3,19 @@ const webpackConfig = require('./webpack.config');
 module.exports = function(config) {
   config.set({
     files: [
-      'tests.webpack.js'
     ],
-    
+
     preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
     },
-    
+
     reporters: ['mocha'],
-    
+
     frameworks: ['mocha', 'chai', 'sinon'],
-    
+
     browsers: ['Chrome'],
-    
+
     webpack: webpackConfig,
-    
+
     webpackServer: {
       noInfo: true
     }
