5d04f8002bed4ec9d7ef9d8446b37b868b48037b
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2017-12-05 13:51:21 -0800

    Log error when failed to reach flags/goals due to incorrect env key

--- src/Requestor.js
+++ src/Requestor.js
@@ -4,7 +4,7 @@ var json = 'application/json';
 
 function fetchJSON(endpoint, callback) {
   var xhr = new XMLHttpRequest();
-  
+
   xhr.addEventListener('load', function() {
     if (xhr.status === 200 && xhr.getResponseHeader('Content-type') === json) {
       callback(null, JSON.parse(xhr.responseText));
@@ -12,14 +12,27 @@ function fetchJSON(endpoint, callback) {
       callback(xhr.statusText);
     }
   });
-  
+
   xhr.addEventListener('error', function() {
+    if(xhr.status === 404) {
+      var error
+      if(endpoint.includes('/sdk/eval/')) {
+        error = 'Error fetching flag settings'
+      }
+      else if(endpoint.includes('/sdk/goals/')) {
+        error = 'Error fetching goals'
+      }
+      else {
+        error = 'Error'
+      }
+      console.error(error + ': environment not found. Please see https://docs.launchdarkly.com/docs/js-sdk-reference#section-initializing-the-client for instructions on SDK initialization.')
+    }
     callback(xhr.statusText);
   });
-  
+
   xhr.open('GET', endpoint);
   xhr.send();
-  
+
   return xhr;
 }
 
@@ -28,7 +41,7 @@ var lastFlagSettingsCallback;
 
 function Requestor(baseUrl, environment) {
   var requestor = {};
-  
+
   requestor.fetchFlagSettings = function(user, hash, callback) {
     var data = utils.base64URLEncode(JSON.stringify(user));
     var endpoint = [baseUrl, '/sdk/eval/', environment,  '/users/', data, hash ? '?h=' + hash : ''].join('');
@@ -41,7 +54,7 @@ function Requestor(baseUrl, environment) {
         lastFlagSettingsCallback = null;
       };
     })(callback);
-    
+
 
     if (flagSettingsRequest) {
       flagSettingsRequest.abort();
@@ -58,12 +71,12 @@ function Requestor(baseUrl, environment) {
     lastFlagSettingsCallback = cb;
     flagSettingsRequest = fetchJSON(endpoint, cb);
   };
-  
+
   requestor.fetchGoals = function(callback) {
     var endpoint = [baseUrl, '/sdk/goals/', environment].join('');
     fetchJSON(endpoint, callback);
   };
-  
+
   return requestor;
 }
 
