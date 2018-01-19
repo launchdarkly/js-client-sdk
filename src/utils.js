745b4a244cda2b52996c553855fd723c289a14d1
Alexis Georges <alexis.georges@gmail.com>
2018-01-19 14:45:18 -0500

    polyfill object.assign

--- src/utils.js
+++ src/utils.js
@@ -30,9 +30,35 @@ function modifications(oldObj, newObj) {
   return mods;
 }
 
+// Based off of https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+function merge(target, varArgs) { // .length of function is 2
+  'use strict';
+  
+  if (target == null) { // TypeError if undefined or null
+    throw new TypeError('Cannot convert undefined or null to object');
+  }
+
+  var to = Object(target);
+
+  for (var index = 1; index < arguments.length; index++) {
+    var nextSource = arguments[index];
+
+    if (nextSource != null) { // Skip over if undefined or null
+      for (var nextKey in nextSource) {
+        // Avoid bugs when hasOwnProperty is shadowed
+        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+          to[nextKey] = nextSource[nextKey];
+        }
+      }
+    }
+  }
+  return to;
+}
+
 module.exports = {
   btoa: btoa,
   base64URLEncode: base64URLEncode,
   clone: clone,
-  modifications: modifications
+  modifications: modifications,
+  merge: merge
 };
