7d13ef631a4d81f7d995b47af07167661f4f42c1
Alexis Georges <alexis.georges@gmail.com>
2018-01-23 13:59:58 -0500

    fix promise interface

--- src/utils.js
+++ src/utils.js
@@ -55,10 +55,41 @@ function merge(target, varArgs) { // .length of function is 2
   return to;
 }
 
+/**
+ * Wrap a promise to invoke an optional callback upon resolution or rejection.
+ * 
+ * This function assumes the callback follows the Node.js callback type: (err, value) => void
+ * 
+ * If a callback is provided:
+ *   - if the promise is resolved, invoke the callback with (null, value)
+ *   - if the promise is rejected, invoke the callback with (error, null)
+ * 
+ * @param {Promise<any>} promise 
+ * @param {Function} callback 
+ * @returns Promise<any>
+ */
+function wrapPromiseCallback(promise, callback) {
+  return promise.then(
+    function(value) {
+      if (callback) {
+        setTimeout(function() { callback(null, value); }, 0);
+      }
+      return value;
+    },
+    function(error) {
+      if (callback) {
+        setTimeout(function() { callback(error, null); }, 0);
+      }
+      return Promise.reject(error);
+    }
+  );
+}
+
 module.exports = {
   btoa: btoa,
   base64URLEncode: base64URLEncode,
   clone: clone,
   modifications: modifications,
-  merge: merge
+  merge: merge,
+  wrapPromiseCallback: wrapPromiseCallback
 };
