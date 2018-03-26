7bf72b3db332db28160870243fe6edd02239d53b
Alexis Georges <alexis.georges@gmail.com>
2018-03-26 16:10:37 -0400

    make sure to attach history listeners once we receive goals

--- src/index.js
+++ src/index.js
@@ -343,6 +343,23 @@ function initialize(env, user, options) {
     });
   }
 
+  function refreshGoalTracker() {
+    if (goalTracker) {
+      goalTracker.dispose();
+    }
+    if (goals && goals.length) {
+      goalTracker = GoalTracker(goals, sendGoalEvent);
+    }
+  }
+
+  function attachHistoryListeners() {
+    if (!!(window.history && history.pushState)) {
+      window.addEventListener('popstate', refreshGoalTracker);
+    } else {
+      window.addEventListener('hashchange', refreshGoalTracker);
+    }
+  }
+
   requestor.fetchGoals(function(err, g) {
     if (err) {
       emitter.maybeReportError(new errors.LDUnexpectedResponseError('Error fetching goals: ' + err.message ? err.message : err));
@@ -350,6 +367,7 @@ function initialize(env, user, options) {
     if (g && g.length > 0) {
       goals = g;
       goalTracker = GoalTracker(goals, sendGoalEvent);
+      attachHistoryListeners();
     }
   });
 
@@ -371,23 +389,6 @@ function initialize(env, user, options) {
   window.addEventListener('beforeunload', function() {
     events.flush(ident.getUser(), true);
   });
-
-  function refreshGoalTracker() {
-    if (goalTracker) {
-      goalTracker.dispose();
-    }
-    if (goals && goals.length) {
-      goalTracker = GoalTracker(goals, sendGoalEvent);
-    }
-  }
-
-  if (goals && goals.length > 0) {
-    if (!!(window.history && history.pushState)) {
-      window.addEventListener('popstate', refreshGoalTracker);
-    } else {
-      window.addEventListener('hashchange', refreshGoalTracker);
-    }
-  }
 
   window.addEventListener('message', handleMessage);
 
