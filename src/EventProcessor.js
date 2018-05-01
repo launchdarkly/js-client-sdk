7f56026b024288b344f500a797c729936adf386d
Jay Winstead <jwinstead@launchdarkly.com>
2018-05-01 12:09:41 -0700

    move XMLHttpRequest into send method

--- src/EventProcessor.js
+++ src/EventProcessor.js
@@ -1,16 +1,17 @@
 import * as utils from './utils';
 
 const MAX_URL_LENGTH = 2000;
-const hasCors = 'withCredentials' in new XMLHttpRequest();
 
 function sendEvents(eventsUrl, events, sync) {
   const src = eventsUrl + '?d=' + utils.base64URLEncode(JSON.stringify(events));
 
   const send = onDone => {
+    const xhr = new XMLHttpRequest();
+    const hasCors = 'withCredentials' in xhr;
+
     // Detect browser support for CORS
     if (hasCors) {
       /* supports cross-domain requests */
-      const xhr = new XMLHttpRequest();
       xhr.open('GET', src, !sync);
 
       if (!sync) {
