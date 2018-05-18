fa41287742ce25a6ff4e2afe1fc145459bd9ce93
Jay Winstead <jwinstead@launchdarkly.com>
2018-05-18 15:22:01 -0700

    add reconnect logic to stream

--- src/Stream.js
+++ src/Stream.js
@@ -3,11 +3,13 @@ import { base64URLEncode } from './utils';
 export default function Stream(baseUrl, environment, hash, useReport) {
   const stream = {};
   const evalUrlPrefix = baseUrl + '/eval/' + environment + '/';
+  let reconnectTimeoutReference;
   let es = null;
 
   stream.connect = function(user, handlers) {
     if (typeof EventSource !== 'undefined') {
       let url;
+      clearTimeout(reconnectTimeoutReference);
       if (useReport) {
         // we don't yet have an EventSource implementation that supports REPORT, so
         // fall back to the old ping-based stream
@@ -19,6 +21,12 @@ export default function Stream(baseUrl, environment, hash, useReport) {
         }
       }
       es = new window.EventSource(url);
+      es.onerror = () => {
+        reconnectTimeoutReference = setTimeout(() => {
+          this.disconnect();
+          this.connect(user, handlers);
+        }, 5000);
+      };
       for (const key in handlers) {
         if (handlers.hasOwnProperty(key)) {
           es.addEventListener(key, handlers[key]);
@@ -28,6 +36,7 @@ export default function Stream(baseUrl, environment, hash, useReport) {
   };
 
   stream.disconnect = function() {
+    clearTimeout(reconnectTimeoutReference);
     if (es) {
       es.close();
     }
