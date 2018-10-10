f95c5d2ff51cc556fb00f52711810d44bc16423c
Eli Bishop <eli@launchdarkly.com>
2018-10-09 17:08:14 -0700

    add desultory test

---
+++ packages/ldclient-js/src/__tests__/LDClient-test.js
@@ -0,0 +1,53 @@
+import sinon from 'sinon';
+
+import * as LDClient from '../index';
+
+describe('LDClient', () => {
+  const envName = 'UNKNOWN_ENVIRONMENT_ID';
+  const user = { key: 'user' };
+  let warnSpy;
+  let errorSpy;
+  let xhr;
+  let requests = [];
+
+  beforeEach(() => {
+    xhr = sinon.useFakeXMLHttpRequest();
+    xhr.onCreate = function(req) {
+      requests.push(req);
+    };
+
+    warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+    errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    requests = [];
+    xhr.restore();
+    warnSpy.mockRestore();
+    errorSpy.mockRestore();
+  });
+
+  function getLastRequest() {
+    return requests[requests.length - 1];
+  }
+
+  it('should exist', () => {
+    expect(LDClient).toBeDefined();
+  });
+
+  describe('initialization', () => {
+    it('should trigger the ready event', done => {
+      const handleReady = jest.fn();
+      const client = LDClient.initialize(envName, user, {
+        bootstrap: {},
+      });
+
+      client.on('ready', handleReady);
+
+      setTimeout(() => {
+        expect(handleReady).toHaveBeenCalled();
+        done();
+      }, 0);
+    });
+  });
+});
