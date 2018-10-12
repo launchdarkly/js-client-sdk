5fefab5be48e1bf61ba03f7abcc9928fcc84a46d
Eli Bishop <eli@launchdarkly.com>
2018-10-12 12:30:59 -0700

    tolerate lack of EventSource

--- packages/ldclient-js/src/browserPlatform.js
+++ packages/ldclient-js/src/browserPlatform.js
@@ -29,7 +29,10 @@ export default function makeBrowserPlatform() {
     return flag === 1 || flag === true || flag === '1' || flag === 'yes';
   };
 
-  ret.eventSourceFactory = url => new window.EventSource(url);
+  // If EventSource does not exist, the absence of eventSourceFactory will make us not try to open streams
+  if (window.EventSource) {
+    ret.eventSourceFactory = url => new window.EventSource(url);
+  }
 
   ret.eventSourceAllowsReport = false;
 
