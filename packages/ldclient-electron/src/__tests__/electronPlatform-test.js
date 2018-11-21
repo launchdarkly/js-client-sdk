7f02aea0391a804b5535e0ff66d4455132fa6bfa
Eli Bishop <eli@launchdarkly.com>
2018-11-21 10:24:47 -0800

    add Electron storage tests

--- packages/ldclient-electron/src/__tests__/electronPlatform-test.js
+++ packages/ldclient-electron/src/__tests__/electronPlatform-test.js
@@ -2,6 +2,7 @@ import electronPlatform from '../electronPlatform';
 
 describe('electronPlatform', () => {
   const platform = electronPlatform();
+  const lsKeyPrefix = 'ldclient-electron-test:';
 
   describe('getCurrentUrl()', () => {
     it('returns null', () => {
@@ -12,6 +13,43 @@ describe('electronPlatform', () => {
   describe('isDoNotTrack()', () => {
     it('returns false', () => {
       expect(platform.isDoNotTrack()).toEqual(false);
+    });
+  });
+
+  describe('local storage', () => {
+    it('returns null for missing value', done => {
+      platform.localStorage.get(lsKeyPrefix + 'unused-key', (err, value) => {
+        expect(err).toBe(null);
+        expect(value).toBe(null);
+        done();
+      });
+    });
+
+    it('can get and set value', done => {
+      const key = lsKeyPrefix + 'get-set-key';
+      platform.localStorage.set(key, 'hello', err => {
+        expect(err).toBe(null);
+        platform.localStorage.get(key, (err, value) => {
+          expect(err).toBe(null);
+          expect(value).toEqual('hello');
+          done();
+        });
+      })
+    });
+
+    it('can delete value', done => {
+      const key = lsKeyPrefix + 'delete-key';
+      platform.localStorage.set(key, 'hello', err => {
+        expect(err).toBe(null);
+        platform.localStorage.clear(key, err => {
+          expect(err).toBe(null);
+          platform.localStorage.get(key, (err, value) => {
+            expect(err).toBe(null);
+            expect(value).toBe(null);
+            done();
+          });
+        });
+      });
     });
   });
 });
