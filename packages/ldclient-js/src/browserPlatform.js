4fbd0f7dc692d1eaeed1dce685e95ddad1924ddf
Eli Bishop <eli@launchdarkly.com>
2018-11-25 18:01:37 -0800

    always set flag

--- packages/ldclient-js/src/browserPlatform.js
+++ packages/ldclient-js/src/browserPlatform.js
@@ -77,8 +77,9 @@ export default function makeBrowserPlatform() {
     ret.eventSourceIsActive = es =>
       es.readyState === window.EventSource.OPEN || es.readyState === window.EventSource.CONNECTING;
 
-    ret.eventSourceAllowsReport = false;
   }
+
+  ret.eventSourceAllowsReport = false;
 
   return ret;
 }
