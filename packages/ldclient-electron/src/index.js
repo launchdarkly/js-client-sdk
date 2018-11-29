bb6ae365f38e8f12fff132184ef3b551b2d4e7f0
Eli Bishop <eli@launchdarkly.com>
2018-11-28 21:30:44 -0800

    add Node SDK emulation mode in Electron

--- packages/ldclient-electron/src/index.js
+++ packages/ldclient-electron/src/index.js
@@ -2,13 +2,14 @@ import * as browserClient from 'ldclient-js';
 import * as common from 'ldclient-js-common';
 import electronPlatform from './electronPlatform';
 import * as interprocessSync from './interprocessSync';
+import makeNodeSdkClientWrapper from './nodeSdkEmulation';
 
 // This creates an SDK instance to be used in the main process of Electron. It can be used
 // either by itself or in combination with SDK instances in renderer windows (created with
 // initializeRenderer).
 export function initializeMain(env, user, options = {}) {
   // Pass our platform object to the common code to create the Electron version of the client
-  const platform = electronPlatform();
+  const platform = electronPlatform(options);
   const clientVars = common.initialize(env, user, options, platform);
   const client = clientVars.client;
 
@@ -20,14 +21,39 @@ export function initializeMain(env, user, options = {}) {
   client.on(clientVars.internalChangeEventName, tracker.updatedFlags);
 
   const realIdentify = client.identify;
-  client.identify = (user, cb) => {
+  client.identify = (user, hash, cb) => {
     tracker.changedUser(user);
-    realIdentify(user, cb);
+    return realIdentify(user, hash, cb);
+  };
+
+  client.close = () => clientVars.stop();
+
+  // This method is probably not of much use in Electron since we have a better way to send flag
+  // data to the front end, but it exists in all the server-side SDKs so why not.
+  client.allFlagsState = () => {
+    const flags = clientVars.getFlagsInternal();
+    const result = {};
+    const metadata = {};
+    Object.keys(flags).forEach(key => {
+      const flagState = Object.assign({}, flags[key]);
+      result[key] = flagState.value;
+      delete flagState.value;
+      metadata[key] = flagState;
+    });
+    result['$flagsState'] = metadata;
+    result['$valid'] = true;
+    return result;
   };
 
   clientVars.start();
 
   return clientVars.client;
+}
+
+// See nodeSdkEmulation.js
+export function initializeMainWithNodeApi(env, user, options = {}) {
+  const client = initializeMain(env, user, options);
+  return makeNodeSdkClientWrapper(client, user);
 }
 
 export function initializeInRenderer(optionalEnv, options = {}) {
---
+++ packages/ldclient-electron/src/nodeSdkEmulation.js
@@ -0,0 +1,86 @@
+import * as common from 'ldclient-js-common';
+
+// This simply wraps the regular Electron client in an API that is more like the Node SDK, to assist
+// developers who are transitioning from the Node SDK.
+//
+// Note that since the underlying client still uses the single-current-user client-side model, we
+// have to call identify() prior to every flag evaluation. Therefore, this will perform very poorly
+// if for some reason you try to evaluate flags for a bunch of different users.
+
+export default function makeNodeSdkClientWrapper(realClient, initialUser) {
+  let initComplete = false;
+
+  realClient.on('ready', () => {
+    initComplete = true;
+  });
+
+  let currentUser;
+
+  function setCurrentUser(user) {
+    currentUser = Object.assign({}, user);
+  }
+
+  setCurrentUser(initialUser);
+
+  function maybeChangeUser(user) {
+    // TODO: this should be a deep equality check - eventually that will be done within identify()
+    if (user && currentUser && user.key === currentUser.key) {
+      return Promise.resolve();
+    }
+    setCurrentUser(user);
+    return realClient.identify(user);
+  }
+
+  function withUserAsync(user, callback, resultFn) {
+    return common.utils.wrapPromiseCallback(maybeChangeUser(user).then(() => resultFn()), callback);
+  }
+
+  const wrapper = {
+    initialized: () => initComplete,
+
+    // In the Node SDK, waitUntilReady() is a deprecated method that resolves its Promise if
+    // initialization succeeds *or fails*.
+    waitUntilReady: () => realClient.waitForInitialization().catch(() => Promise.resolve()),
+
+    waitForInitialization: () => realClient.waitForInitialization(),
+
+    variation: (key, user, defaultVal, callback) =>
+      withUserAsync(user, callback, () => realClient.variation(key, defaultVal)),
+
+    variationDetail: (key, user, defaultVal, callback) =>
+      withUserAsync(user, callback, () => realClient.variationDetail(key, defaultVal)),
+
+    allFlags: (user, callback) => withUserAsync(user, callback, () => realClient.allFlags()),
+
+    allFlagsState: (user, callback) => withUserAsync(user, callback, () => realClient.allFlagsState()),
+
+    track: (eventName, user, data) =>
+      // Note that track() does not actually return a Promise in the Node SDK, but it's helpful for testing
+      withUserAsync(user, null, () => {
+        realClient.track(eventName, data);
+      }).catch(() => {}), // suppress errors because the Promise will not normally be used
+
+    identify: user => {
+      setCurrentUser(user);
+      realClient.identify(user).catch(() => {});
+    },
+
+    secureModeHash: () => {
+      console.warn('LDClient.secureModeHash is not supported in the Electron SDK');
+      return '';
+    },
+
+    close: () => realClient.close(),
+
+    // TODO: we don't currently have an offline mode
+    isOffline: () => false,
+
+    flush: callback => realClient.flush(callback),
+
+    on: (...args) => realClient.on(...args),
+
+    off: (...args) => realClient.off(...args),
+  };
+
+  return wrapper;
+}
