b2a3ae7f10d788137dc5ff7ea801110455851550
Eli Bishop <eli@launchdarkly.com>
2019-02-12 18:17:15 -0800

    add release script that releases all packages in js-client

---
+++ scripts/release.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# This script updates the version for the js-client packages and releases them to npm.
+# It requires you to have valid NPM credentials associated with the packages.
+
+# It takes exactly one argument: the new version.
+# It should be run from the root of this git repo like this:
+#   ./scripts/release.sh 4.0.9
+
+# When done you should commit and push the changes made.
+
+set -uxe
+echo "Starting js-client release."
+
+VERSION=$1
+
+PROJECT_DIR=`pwd`
+
+# Make the lerna command available.
+
+npm install
+
+# Update version in all packages.
+
+./node_modules/.bin/lerna version $VERSION --no-git-tag-version --no-push -y
+
+# Publish all packages.
+#
+# Supposedly, we should be able to do them all at once like so:
+#    ./node_modules/.bin/lerna publish $VERSION --from-package --no-git-reset --no-git-tag-version -y
+#
+# However, we haven't been able to get that to work. The packages get built, but the tarballs uploaded
+# to npm do not contain the build products. In other words, it is *not* equivalent to just running
+# "npm publish" in each package directory. So, for now, we'll just do the latter.
+
+for package in ldclient-js-common ldclient-js ldclient-react; do
+  cd $PROJECT_DIR/packages/$package
+  npm publish
+done
+
+echo "Done with js-client release"
