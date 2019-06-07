89f7f75f85131a4570967fa6c55c15244948e5ce
Eli Bishop <eli@launchdarkly.com>
2019-06-06 20:31:51 -0700

    prepare 2.11.0 release (#157)

--- packages/launchdarkly-react-client-sdk/example/src/universal/app.js
+++ packages/launchdarkly-react-client-sdk/example/src/universal/app.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { withLDProvider } from 'launchdarkly-react-client-sdk';
+import SiteNav from './siteNav';
 import Home from './home';
+import HooksDemo from './hooksDemo';
 
 const App = () => (
   <div>
+    <SiteNav />
     <main>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/home">
           <Redirect to="/" />
         </Route>
+        <Route path="/hooks" component={HooksDemo} />
       </Switch>
     </main>
   </div>
--- packages/launchdarkly-react-client-sdk/src/index.ts
+++ packages/launchdarkly-react-client-sdk/src/index.ts
@@ -1,4 +1,6 @@
 import withLDProvider from './withLDProvider';
 import withLDConsumer from './withLDConsumer';
+import useFlags from './useFlags';
+import useLDClient from './useLDClient';
 
-export { withLDProvider, withLDConsumer };
+export { withLDProvider, withLDConsumer, useFlags, useLDClient };
