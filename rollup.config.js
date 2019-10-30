bdc72e70864564c193596ebea038a2217c2a339a
Eli Bishop <eli@launchdarkly.com>
2019-10-29 21:29:45 -0700

    unify Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,49 @@
-const common = require('./rollup.common.config');
 const pkg = require('./package.json');
 
-const config = Object.assign({}, common, {
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
+const replace = require('rollup-plugin-replace');
+const uglify = require('rollup-plugin-uglify');
+const builtins = require('rollup-plugin-node-builtins');
+const globals = require('rollup-plugin-node-globals');
+const filesize = require('rollup-plugin-filesize');
+
+const env = process.env.NODE_ENV || 'development';
+const version = process.env.npm_package_version;
+
+let plugins = [
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(env),
+    VERSION: JSON.stringify(version),
+  }),
+  globals(),
+  builtins(),
+  resolve({
+    browser: true,
+    module: true,
+    jsnext: true,
+    main: true,
+    preferBuiltins: true,
+  }),
+  commonjs(),
+  babel(),
+  filesize(),
+];
+
+if (env === 'production') {
+  plugins = plugins.concat(
+    uglify({
+      compress: {},
+    })
+  );
+}
+
+const config = {
   input: 'src/index.js',
   output: [
     {
-      plugins: common.plugins,
+      plugins: plugins,
       name: 'LDClient',
       file: process.env.NODE_ENV === 'production' ? './dist/ldclient.min.js' : './dist/ldclient.js',
       format: 'umd',
@@ -14,6 +52,7 @@ const config = Object.assign({}, common, {
     { file: pkg.main, format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
-});
+  plugins: plugins,
+};
 
 module.exports = config;
