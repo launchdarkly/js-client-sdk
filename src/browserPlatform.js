f3330a767ff635e83b3386a6380162b6af9038a2
Eli Bishop <eli@launchdarkly.com>
2019-12-11 18:10:52 -0800

    enable diagnostic events in JS SDK

--- src/browserPlatform.js
+++ src/browserPlatform.js
@@ -120,6 +120,18 @@ export default function makeBrowserPlatform(options) {
   }
 
   ret.userAgent = 'JSClient';
+  ret.version = VERSION;
+
+  ret.diagnosticSdkData = {
+    name: 'JS',
+    version: VERSION,
+  };
+
+  ret.diagnosticPlatformData = {
+    name: 'JS',
+  };
+
+  ret.diagnosticUseCombinedEvent = true; // the browser SDK uses the "diagnostic-combined" event format
 
   return ret;
 }
