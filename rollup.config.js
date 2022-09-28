ce75d7fe4251c1331539176632b5e30849e07dc2
Nat Welch <nat@natwelch.com>
2022-09-28 22:09:14 +0000

    fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 const pkg = require('./package.json');
 const resolve = require('@rollup/plugin-node-resolve');
-const commonjs = require('rollup/plugin-commonjs');
+const commonjs = require('@rollup/plugin-commonjs');
 const babel = require('@rollup/plugin-babel');
 const replace = require('@rollup/plugin-replace');
 const { terser } = require('rollup-plugin-terser');
